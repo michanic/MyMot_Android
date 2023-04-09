b18d36a39236d5ce65daa7b84ea7ff2267d48578
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-04-09 20:52:38 +0300

    Error SitesInteractor

--- app/src/main/java/ru/michanic/mymot/Kotlin/Protocols/LoadingAdvertsInterface.kt
+++ app/src/main/java/ru/michanic/mymot/Kotlin/Protocols/LoadingAdvertsInterface.kt
@@ -2,7 +2,7 @@ package ru.michanic.mymot.Kotlin.Protocols
 
 import ru.michanic.mymot.Kotlin.Models.Advert
 
-interface LoadingAdvertsInterface {
-    fun onLoaded(adverts: List<Advert?>?, loadMore: Boolean)
-    fun onFailed()
-}
+//interface LoadingAdvertsInterface {
+//    fun onLoaded(adverts: List<Advert?>?, loadMore: Boolean)
+//    fun onFailed()
+//}
