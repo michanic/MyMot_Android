aae3f088fd5e4ab5b3bd10b19bac34f43ac20538
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-07-29 16:05:50 +0300

    Set text RangeSlider in EditText

--- app/src/main/java/ru/michanic/mymot/Kotlin/UI/Activities/CatalogFilterActivity.kt
+++ app/src/main/java/ru/michanic/mymot/Kotlin/UI/Activities/CatalogFilterActivity.kt
@@ -1,5 +1,6 @@
 package ru.michanic.mymot.Kotlin.UI.Activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.MenuItem
 import ru.michanic.mymot.Kotlin.MyMotApplication
@@ -19,6 +20,65 @@ class CatalogFilterActivity : UniversalActivity() {
         setNavigationTitle("Фильтр")
         goBackOnSearch = intent.getBooleanExtra("goBackOnSearch", false)
 
+        yearsRelease()
+        cylinderCount()
+        engineDisplacement()
+        maxPower()
+        enginePrm()
+        seatHeight()
+        curbWeight()
+    }
+
+    private fun yearsRelease() {
+        binding.yearsReleaseSlider.addOnChangeListener { slider, value, fromUser ->
+            val values = binding.yearsReleaseSlider.values
+            binding.yearsReleaseOfEt.setText("от : ${values[0]}")
+            binding.yearsReleaseToEt.setText("до : ${values[1]}")
+        }
+    }
+
+    private fun engineDisplacement() {
+        binding.engineDisplacementSlider.addOnChangeListener { slider, value, fromUser ->
+            val values = binding.engineDisplacementSlider.values
+            binding.engineDisplacementOfEt.setText("от : ${values[0]}")
+            binding.engineDisplacementToEt.setText("до : ${values[1]}")
+        }
+    }
+
+    private fun maxPower() {
+        binding.maxPowerSlider.addOnChangeListener { slider, value, fromUser ->
+            val values = binding.maxPowerSlider.values
+            binding.maxPowerOfEt.setText("от : ${values[0]}")
+            binding.maxPowerToEt.setText("до : ${values[1]}")
+        }
+    }
+
+    private fun enginePrm() {
+        binding.enginePrmSlider.addOnChangeListener { slider, value, fromUser ->
+            val values = binding.enginePrmSlider.values
+            binding.engineRpmOfEt.setText("от : ${values[0]}")
+            binding.engineRpmToEt.setText("до : ${values[1]}")
+        }
+    }
+
+    private fun seatHeight() {
+        binding.seatHeightSlider.addOnChangeListener { slider, value, fromUser ->
+            val values = binding.seatHeightSlider.values
+            binding.seatHeightOfEt.setText("от : ${values[0]}")
+            binding.seatHeightToEt.setText("до : ${values[1]}")
+        }
+    }
+
+    private fun curbWeight() {
+        binding.curbWeightSlider.addOnChangeListener { slider, value, fromUser ->
+            val values = binding.curbWeightSlider.values
+            binding.curbWeightOfEt.setText("от : ${values[0]}")
+            binding.curbWeightToEt.setText("до : ${values[1]}")
+        }
+    }
+
+
+    private fun cylinderCount() {
         binding.cylinderCountSlider.addOnChangeListener { slider, value, fromUser ->
             binding.cylinderCountSliderTv.text = value.toInt().toString()
         }
